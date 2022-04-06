6099a5cbf0d40c1b9aeb48639cb7560a55018474
Shane Claussen <sclaussen@yahoo.com>
2022-04-06 08:57:56 -0700

    Initial Commit

---
+++ Barcode/BarcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeApp.swift
+//  Barcode
+//
+//  Created by Shane Claussen on 4/6/22.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Barcode/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Barcode
+//
+//  Created by Shane Claussen on 4/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
